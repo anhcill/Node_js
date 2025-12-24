d5cc841658aa3c88035913b42310f1540fb1172e
anhcill <11220230@st.neu.edu.vn>
2025-12-24 11:46:16 +0700

    Bai hoc buoi 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+
+const port = 3055;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Exit server Express");
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const helmet = require("helmet");
+const compression = require("compression");
+
+// init middleware
+app.use(morgan("dev"));
+// bao ve trang web ko cho xem lam bang gi
+app.use(helmet());
+// giam dung luong v
+app.use(compression());
+// product
+// morgan("combined")
+// morgan("common")
+// morgan("dev")
+// morgan("short ")
+// morgan("tiny")
+app.get("/", (req, res, next) => {
+  const strCompans = "halo";
+  return res.status(200).json({
+    message: "welcome js!",
+    metadata: strCompans.repeat(100000),
+  });
+});
+// init db
+
+// init routes
+
+module.exports = app;
